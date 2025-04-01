87f5f68ce9bc8fa4494955e25b297d538b1fa309
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-01 18:48:45 +0900

    Docs: 06 부트스트랩 제외 기본문제 코드 추가

---
+++ 2nd_study/김하연/07_VUE/기본/style-test/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="$style.child">
+    <h2>Child3</h2>
+    <!-- <Child1 />
+    <Child2 />
+    <Child3 /> -->
+  </div>
+</template>
+<script>
+// import Child1 from './components/Child1.vue';
+// import Child2 from './components/Child2.vue';
+// import Child3 from './components/Child3.vue';
+export default {
+  // name: 'App',
+  // components: { Child1, Child2, Child3 },
+  name: 'Child3',
+  created() {
+    console.log(this.$style.child);
+  },
+};
+</script>
+<style module>
+.child {
+  background-color: orange;
+  border: solid 1px black;
+  margin: 1.5rem;
+  padding: 1rem;
+}
+</style>
